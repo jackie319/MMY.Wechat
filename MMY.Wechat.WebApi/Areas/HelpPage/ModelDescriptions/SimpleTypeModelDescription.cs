4a26aa778b3b7f1a0065d249ad898df31fb917c4
jackie319 <jackie319@vip.qq.com>
2017-07-04 21:34:40 +0800

    1111

---
+++ MMY.Wechat.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MMY.Wechat.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MMY.Wechat.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MMY.Wechat.WebApi.WebApiApplication" Language="C#" %>
