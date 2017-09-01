29b226f6faf79a191cde97dc99fc83a3eb4f0b22
jackie319 <jackie319@vip.qq.com>
2017-09-01 19:18:10 +0800

    web api 重定向

--- MMY.Wechat.WebApi/Controllers/ValuesController.cs
+++ MMY.Wechat.WebApi/Controllers/ValuesController.cs
@@ -7,33 +7,45 @@ using System.Web.Http;
 
 namespace MMY.Wechat.WebApi.Controllers
 {
+    [RoutePrefix("Values")]
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //// GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/values/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        public void Delete(int id)
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+
+        /// <summary>
+        /// web api 重定向
+        /// </summary>
+        /// <returns></returns>
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult RedirectResult()
         {
+            return Redirect("http://m.maimaiyin.cn/?#/Home");
         }
     }
 }
